4694a298b52e2b12dfa7f1fb8f0e98787326b40c
Manuel Giffels <giffels@gmail.com>
2022-04-25 17:28:59 +0200

    Use separate htcondor stage directory

--- dask_gateway_htcondor/htcondor.py
+++ dask_gateway_htcondor/htcondor.py
@@ -4,7 +4,7 @@ from dask_gateway_server.traitlets import Type
 from traitlets import Dict, Unicode, default
 
 from enum import Enum
-import math, os, re, shutil
+import math, os, pwd, re, shutil
 
 def htcondor_create_execution_script(execution_script, setup_command, execution_command):
     # write script to staging_dir
@@ -54,6 +54,19 @@ class HTCondorClusterConfig(JobQueueClusterConfig):
     universe = Unicode("docker", help="The universe to submit jobs to. Defaults to docker", config=True)
     docker_image = Unicode("coffeateam/coffea-dask-cc7-gateway", help="The docker image to run jobs in.", config=True)
     extra_jdl = Dict(help="Additional content of the job description file.", config=True)
+    htcondor_staging_directory = Unicode(
+    "{home}/.dask-gateway-htcondor/",
+    help="""
+    The htcondor staging directory for storing files before the job starts.
+    A subdirectory will be created for each new cluster which will store
+    temporary files for that cluster. On cluster shutdown the subdirectory
+    will be removed.
+    This field can be a template, which receives the following fields:
+    - home (the user's home directory)
+    - username (the user's name)
+    """,
+    config=True,
+)
 
 
 class HTCondorBackend(JobQueueBackend):
@@ -78,33 +91,39 @@ class HTCondorBackend(JobQueueBackend):
     def _default_status_command(self):
         return shutil.which("condor_q") or "condor_q"
 
+    def _get_htcondor_staging_dir(cluster):
+        htcondor_staging_dir = cluster.config.htcondor_staging_directory.format(
+            home=pwd.getpwnam(cluster.username).pw_dir, username=cluster.username
+        )
+        return os.path.join(htcondor_staging_dir, cluster.name)
+
     def get_submit_cmd_env_stdin(self, cluster, worker=None):
         cmd = [self.submit_command, "--verbose"]
-        staging_dir = self.get_staging_directory(cluster)
+        htcondor_staging_dir = self._get_htcondor_staging_dir(cluster)
 
         # ensure that staging_dir exists
-        os.makedirs(staging_dir, exist_ok=True)
+        os.makedirs(htcondor_staging_dir, exist_ok=True)
 
         if worker:
-            execution_script = os.path.join(staging_dir, f"run_worker_{worker.name}.sh")
+            execution_script = os.path.join(htcondor_staging_dir, f"run_worker_{worker.name}.sh")
             htcondor_create_execution_script(execution_script=execution_script,
                 setup_command=cluster.config.worker_setup,
                 execution_command=" ".join(self.get_worker_command(cluster, worker.name)))
             env = self.get_worker_env(cluster)
             jdl = htcondor_create_jdl(cluster_config=cluster.config, 
                 execution_script=execution_script,
-                log_dir=os.path.join(staging_dir, "logs_worker_{worker.name}"),
+                log_dir=os.path.join(htcondor_staging_dir, f"logs_worker_{worker.name}"),
                 cpus=cluster.config.worker_cores, 
                 mem=htcondor_memory_format(cluster.config.worker_memory))
         else:
-            execution_script = os.path.join(staging_dir, f"run_scheduler_{cluster.name}.sh")
+            execution_script = os.path.join(htcondor_staging_dir, f"run_scheduler_{cluster.name}.sh")
             htcondor_create_execution_script(execution_script=execution_script,
                 setup_command=cluster.config.scheduler_setup,
                 execution_command=" ".join(self.get_scheduler_command(cluster)))
             env = self.get_scheduler_env(cluster)
             jdl = htcondor_create_jdl(cluster_config=cluster.config,
                 execution_script=execution_script,
-                log_dir=os.path.join(staging_dir, "logs_scheduler_{cluster.name}"),
+                log_dir=os.path.join(htcondor_staging_dir, f"logs_scheduler_{cluster.name}"),
                 cpus=cluster.config.scheduler_cores,
                 mem=htcondor_memory_format(cluster.config.scheduler_memory))
 
