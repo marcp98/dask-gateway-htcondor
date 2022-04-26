ca3d722e35073ed0c9ea5f63e3410285243130bf
Manuel Giffels <giffels@gmail.com>
2022-04-26 17:02:21 +0200

    Use docker host network

--- dask_gateway_htcondor/htcondor.py
+++ dask_gateway_htcondor/htcondor.py
@@ -28,6 +28,7 @@ def htcondor_create_jdl(cluster_config, execution_script, log_dir, cpus, mem, en
     jdl_dict = {"universe": cluster_config.universe,
     "docker_image": cluster_config.docker_image,
     "executable": os.path.relpath(execution_script),
+    "docker_network_type": "host",
     "should_transfer_files": "YES",
     "transfer_input_files": ",".join(tls_path),
     "when_to_transfer_output": "ON_EXIT",
